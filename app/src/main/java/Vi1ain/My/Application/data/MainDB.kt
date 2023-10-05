be6946984fd8440779081c567a2cf6d9ade18e51
Your Name <vilain140e@gmail.com>
2023-10-05 15:33:36 +0500

    first commit

--- app/src/main/java/Vi1ain/My/Application/data/MainDB.kt
+++ app/src/main/java/Vi1ain/My/Application/data/MainDB.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 
 @Database(entities = [ShoppingListItem::class, NoteItem::class, AddItem::class], version = 1)
 abstract class MainDB : RoomDatabase() {
-    abstract class shoppingListDao() : ShoppingListDao
-    abstract class noteDao() : NoteDao
-    abstract class addItemDao() : AddItemDao
+    abstract val shoppingListDao: ShoppingListDao
+    abstract val noteDao: NoteDao
+    abstract val addItemDao: AddItemDao
 }
--- app/src/main/java/Vi1ain/My/Application/di/AppModule.kt
+++ app/src/main/java/Vi1ain/My/Application/di/AppModule.kt
@@ -1,10 +1,46 @@
 package Vi1ain.My.Application.di
 
+import Vi1ain.My.Application.data.AddItemRepoImpl
+import Vi1ain.My.Application.data.AddItemRepository
+import Vi1ain.My.Application.data.MainDB
+import Vi1ain.My.Application.data.NoteItemRepoImpl
+import Vi1ain.My.Application.data.NoteItemRepository
+import Vi1ain.My.Application.data.ShopingListRepoImpl
+import Vi1ain.My.Application.data.ShoppingListRepository
+import android.app.Application
+import androidx.room.Room
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+    @Provides
+    @Singleton
+    fun provideMainDB(app: Application): MainDB {
+        return Room.databaseBuilder(
+            app, MainDB::class.java, "shop_list_db"
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideShoppingListRepository(db: MainDB): ShoppingListRepository {
+        return ShopingListRepoImpl(db.shoppingListDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideAddItemRepository(db: MainDB): AddItemRepository {
+        return AddItemRepoImpl(db.addItemDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideNoteItemRepository(db: MainDB): NoteItemRepository {
+        return NoteItemRepoImpl(db.noteDao)
+    }
 }
